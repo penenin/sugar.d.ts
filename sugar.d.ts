e879897215d271cf01dc0493e5eddea32bfceea0
jbaldwin <joshrbaldwin@gmail.com>
2012-10-20 14:24:09 -0600

    Added number definitions

--- sugar.d.ts
+++ sugar.d.ts
@@ -881,3 +881,658 @@ interface String {
 	***/
 	zenkaku(mode?: string): string;
 }
+
+interface Number {
+
+	/***
+	* @short Returns a random integer between [n1] and [n2].
+	* @method Number.random([n1], [n2])
+	* @returns Number
+	* @extra If only 1 number is passed, the other will be 0. If none are passed, the number will be either 0 or 1.
+	* @example
+	*
+	*   Number.random(50, 100) -> ex. 85
+	*   Number.random(50)      -> ex. 27
+	*   Number.random()        -> ex. 0
+	*
+	***/
+	random(n1?: number, n2?: number): number;
+
+	/***
+	* @short Returns an abbreviated form of the number.
+	* @method abbr([precision] = 0)
+	* @returns String
+	* @extra [precision] will round to the given precision.
+	* @example
+	*
+	*   (1000).abbr()    -> "1k"
+	*   (1000000).abbr() -> "1m"
+	*   (1280).abbr(1)   -> "1.3k"
+	*
+	***/
+	abbr(precision?: number): string;
+
+	/***
+	* @short Returns an abbreviated form of the number, considered to be "Bytes".
+	* @method bytes([precision] = 0, [limit] = 4)
+	* @returns String
+	* @extra [precision] will round to the given precision.
+	*        [limit] is the upper limit for the units.
+	*        The default is %4%, which is "terabytes" (TB).
+	*        If [limit] is %false%, the upper limit will be "exa".
+	* @example
+	*
+	*   (1000).bytes()                 -> "1kB"
+	*   (1000).bytes(2)                -> "0.98kB"
+	*   ((10).pow(20)).bytes()         -> "90,949,470TB"
+	*   ((10).pow(20)).bytes(0, false) -> "87EB"
+	*
+	***/
+	bytes(precision?: number, limit?: number): string;
+	bytes(precision?: number, limit?: bool): string;
+
+	/***
+	* @short Shortcut for %Math.ceil% that also allows a <precision>.
+	* @method ceil(<precision> = 0)
+	* @returns Number
+	*
+	* @example
+	*
+	*   (3.241).ceil()  -> 4
+	*   (-3.241).ceil() -> -3
+	*   (3.241).ceil(2) -> 3.25
+	*   (3748).ceil(-2) -> 3800
+	*
+	***/
+	ceil(precision?: number): number;
+
+	/***
+	* @short Returns a string at the code point of the number.
+	* @method chr()
+	* @returns String
+	* @example
+	*
+	*   (65).chr() -> "A"
+	*   (75).chr() -> "K"
+	*
+	***/
+	chr(): string;
+
+	/***
+	* @short Returns an array containing numbers from the number down to <num>.
+	* @method downto(<num>, [fn], [step] = 1)
+	* @returns Array
+	* @extra Optionally calls [fn] callback for each number in that array.
+	*        [step] allows multiples greater than 1.
+	* @example
+	*
+	*   (8).downto(3) -> [8, 7, 6, 5, 4, 3]
+	*   (8).downto(3, function(n) {
+	*     // This function is called 6 times receiving n as the value.
+	*   });
+	*   (8).downto(2, null, 2) -> [8, 6, 4, 2]
+	*
+	***/
+	downto(num: number, fn?: Function, step?: number): number[];
+
+	/***
+	* @short Takes the number as milliseconds and returns a unit-
+	*        adjusted localized string.
+	* @method duration([locale] = currentLocale)
+	* @returns String
+	* @extra This method is the same as %Date#relative% without 
+	*        the localized equivalent of "from now" or "ago".
+	*        [locale] can be passed as the first (and only) parameter.
+	*        Note that this method is only available when the dates
+	*        package is included.
+	* @example
+	*
+	*   (500).duration() -> '500 milliseconds'
+	*   (1200).duration() -> '1 second'
+	*   (75).minutes().duration() -> '1 hour'
+	*   (75).minutes().duration('es') -> '1 hora'
+	*
+	***/
+	duration(locale?: string): string;
+
+	/***
+	* @short Shortcut for %Math.floor% that also allows a <precision>.
+	* @method floor(<precision> = 0)
+	* @returns Number
+	* @example
+	*
+	*   (3.241).floor()  -> 3
+	*   (-3.841).floor() -> -4
+	*   (3.241).floor(2) -> 3.24
+	*   (3748).floor(-2) -> 3700
+	*
+	***/
+	floor(precision?: number): number;
+
+	/***
+	* @short Formats the number to a readable string.
+	* @method format([place] = 0, [thousands] = ',', [decimal] = '.')
+	* @returns String
+	* @extra If [place] is %undefined%, will automatically determine the place.
+	*        [thousands] is the character used for the thousands separator.
+	*        [decimal] is the character used for the decimal point.
+	* @example
+	*
+	*   (56782).format()           -> '56,782'
+	*   (56782).format(2)          -> '56,782.00'
+	*   (4388.43).format(2, ' ')      -> '4 388.43'
+	*   (4388.43).format(2, '.', ',') -> '4.388,43'
+	*
+	***/
+	format(place?: number, thousands?: string, decimal?: string): string;
+
+	/***
+	* @short Converts the number to hexidecimal.
+	* @method hex([pad] = 1)
+	* @returns String
+	* @extra [pad] will pad the resulting string to that many places.
+	* @example
+	*
+	*   (255).hex()   -> 'ff';
+	*   (255).hex(4)  -> '00ff';
+	*   (23654).hex() -> '5c66';
+	*
+	***/
+	hex(pad?: number): string;
+
+	/***
+	* @short Returns true if the number is even.
+	* @method isEven()
+	* @returns Boolean
+	* @example
+	*
+	*   (6).isEven()  -> true
+	*   (17).isEven() -> false
+	*
+	***/
+	isEven(): bool;
+
+	/***
+	* @short Returns true if the number has no trailing decimal.
+	* @method isInteger()
+	* @returns Boolean
+	* @example
+	*
+	*   (420).isInteger() -> true
+	*   (4.5).isInteger() -> false
+	*
+	***/
+	isInteger(): bool;
+
+	/***
+	* @short Returns true if the number is a multiple of <num>.
+	* @method isMultipleOf(<num>)
+	* @returns Boolean
+	* @example
+	*
+	*   (6).isMultipleOf(2)  -> true
+	*   (17).isMultipleOf(2) -> false
+	*   (32).isMultipleOf(4) -> true
+	*   (34).isMultipleOf(4) -> false
+	*
+	***/
+	isMultipleOf(num: number): bool;
+
+	/***
+	* @short Returns true if the number is odd.
+	* @method isOdd()
+	* @returns Boolean
+	* @example
+	*
+	*   (3).isOdd()  -> true
+	*   (18).isOdd() -> false
+	*
+	***/
+	isOdd(): bool;
+
+	/***
+	* @short Returns the logarithm of the number with base <base>,
+	*        or natural logarithm of the number if <base> is undefined.
+	* @method log(<base> = Math.E)
+	* @returns Number
+	* @example
+	*
+	*   (64).log(2) -> 6
+	*   (9).log(3)  -> 2
+	*   (5).log()   -> 1.6094379124341003
+	*
+	***/
+	log(base?: number): number;
+
+	/***
+	* @short Math related functions are mapped as shortcuts to numbers and are identical. Note that %Number#log% provides some special defaults.
+	* @method [math]()
+	* @returns Number
+	*
+	* @set
+	*   abs
+	*   sin
+	*   asin
+	*   cos
+	*   acos
+	*   tan
+	*   atan
+	*   sqrt
+	*   exp
+	*   pow
+	*
+	* @example
+	*
+	*   (3).pow(3) -> 27
+	*   (-3).abs() -> 3
+	*   (1024).sqrt() -> 32
+	*
+	***/
+	abs(): number;
+	sin(): number;
+	asin(): number;
+	cos(): number;
+	acos(): number;
+	tan(): number;
+	atan(): number;
+	sqrt(): number;
+	exp(): number;
+	pow(num: number): number;
+
+	/***
+	* @short Returns the number as a string in metric notation.
+	* @method metric([precision] = 0, [limit] = 1)
+	* @returns String
+	* @extra [precision] will round to the given precision.
+	*        Both very large numbers and very small numbers are supported.
+	*        [limit] is the upper limit for the units.
+	*        The default is %1%, which is "kilo".
+	*        If [limit] is %false%, the upper limit will be "exa".
+	*        The lower limit is "nano", and cannot be changed.
+	* @example
+	*
+	*   (1000).metric()            -> "1k"
+	*   (1000000).metric()         -> "1,000k"
+	*   (1000000).metric(0, false) -> "1M"
+	*   (1249).metric(2) + 'g'     -> "1.25kg"
+	*   (0.025).metric() + 'm'     -> "25mm"
+	*
+	***/
+	metric(precision?: number, limit?: number): string;
+	metric(precision?: number, limit?: bool): string;
+
+	/***
+	* @short Returns an ordinalized (English) string, i.e. "1st", "2nd", etc.
+	* @method ordinalize()
+	* @returns String
+	* @example
+	*
+	*   (1).ordinalize() -> '1st';
+	*   (2).ordinalize() -> '2nd';
+	*   (8).ordinalize() -> '8th';
+	*
+	***/
+	ordinalize(): string;
+
+	/***
+	* @short Pads a number with "0" to <place>.
+	* @method pad(<place> = 0, [sign] = false, [base] = 10)
+	* @returns String
+	* @extra [sign] allows you to force the sign as well (+05, etc). [base] can change the base for numeral conversion.
+	* @example
+	*
+	*   (5).pad(2)        -> '05'
+	*   (-5).pad(4)       -> '-0005'
+	*   (82).pad(3, true) -> '+082'
+	*
+	***/
+	pad(place?: number, sign?: bool, base?: number): string;
+
+	/***
+	* @short Shortcut for %Math.round% that also allows a <precision>.
+	* @method round(<precision> = 0)
+	* @returns Number
+	*
+	* @example
+	*
+	*   (3.241).round()  -> 3
+	*   (-3.841).round() -> -4
+	*   (3.241).round(2) -> 3.24
+	*   (3748).round(-2) -> 3800
+	*
+	***/
+	round(precision?: number): number;
+
+	/***
+	* @short Calls <fn> a number of times equivalent to the number.
+	* @method times(<fn>)
+	* @returns Number
+	* @example
+	*
+	*   (8).times(function(i) {
+	*     // This function is called 8 times.
+	*   });
+	*
+	***/
+	times(fn: Function): number;
+
+	/***
+	* @short Returns a number. This is mostly for compatibility reasons.
+	* @method toNumber()
+	* @returns Number
+	* @example
+	*
+	*   (420).toNumber() -> 420
+	*
+	***/
+	toNumber(): number;
+
+	/***
+	* @short Takes the number as a corresponding unit of time and
+	*        converts to milliseconds.
+	* @method [unit]()
+	* @returns Number
+	* @extra Method names can be both singular and plural.
+	*        Note that as "a month" is ambiguous as a unit of time,
+	*        %months% will be equivalent to 30.4375 days, the average
+	*        number in a month. Be careful using %months% if you need
+	*        exact precision.
+	*
+	* @set
+	*   millisecond
+	*   milliseconds
+	*   second
+	*   seconds
+	*   minute
+	*   minutes
+	*   hour
+	*   hours
+	*   day
+	*   days
+	*   week
+	*   weeks
+	*   month
+	*   months
+	*   year
+	*   years
+	*
+	* @example
+	*
+	*   (5).milliseconds() -> 5
+	*   (10).hours()       -> 36000000
+	*   (1).day()          -> 86400000
+	*
+	***/
+	millisecond(): number;
+	milliseconds(): number;
+	second(): number;
+	seconds(): number;
+	minute(): number;
+	minutes(): number;
+	hour(): number;
+	hours(): number;
+	day(): number;
+	days(): number;
+	week(): number;
+	weeks(): number;
+	month(): number;
+	months(): number;
+	year(): number;
+	years(): number;
+
+	/***
+	* @short Returns a date <n> units after [d], where <n> is the number.
+	* @method [unit]After([d], [locale] = currentLocale)
+	* @returns Date
+	* @extra [d] will accept a date object, timestamp, or text format.
+	*        Note that "months" is ambiguous as a unit of time. If the
+	*        target date falls on a day that does not exist
+	*        (ie. August 31 -> February 31), the date will be shifted
+	*        to the last day of the month. Be careful using %monthsAfter%
+	*        if you need exact precision. See @date_format for more.
+	*
+	* @set
+	*   millisecondAfter
+	*   millisecondsAfter
+	*   secondAfter
+	*   secondsAfter
+	*   minuteAfter
+	*   minutesAfter
+	*   hourAfter
+	*   hoursAfter
+	*   dayAfter
+	*   daysAfter
+	*   weekAfter
+	*   weeksAfter
+	*   monthAfter
+	*   monthsAfter
+	*   yearAfter
+	*   yearsAfter
+	*
+	* @example
+	*
+	*   (5).daysAfter('tuesday')          -> 5 days after tuesday of this week
+	*   (1).yearAfter('January 23, 1997') -> January 23, 1998
+	*
+	***/
+	millisecondAfter(d: string, locale?: string): Date;
+	millisecondAfter(d: Date, locale?: string): Date;
+	millisecondsAfter(d: string, locale?: string): Date;
+	millisecondsAfter(d: Date, locale?: string): Date;
+	secondAfter(d: string, locale?: string): Date;
+	secondAfter(d: Date, locale?: string): Date;
+	secondsAfter(d: string, locale?: string): Date;
+	secondsAfter(d: Date, locale?: string): Date;
+	minuteAfter(d: string, locale?: string): Date;
+	minuteAfter(d: Date, locale?: string): Date;
+	minutesAfter(d: string, locale?: string): Date;
+	minutesAfter(d: Date, locale?: string): Date;
+	hourAfter(d: string, locale?: string): Date;
+	hourAfter(d: Date, locale?: string): Date;
+	hoursAfter(d: string, locale?: string): Date;
+	hoursAfter(d: Date, locale?: string): Date;
+	dayAfter(d: string, locale?: string): Date;
+	dayAfter(d: Date, locale?: string): Date;
+	daysAfter(d: string, locale?: string): Date;
+	daysAfter(d: Date, locale?: string): Date;
+	weekAfter(d: string, locale?: string): Date;
+	weekAfter(d: Date, locale?: string): Date;
+	weeksAfter(d: string, locale?: string): Date;
+	weeksAfter(d: Date, locale?: string): Date;
+	monthAfter(d: string, locale?: string): Date;
+	monthAfter(d: Date, locale?: string): Date;
+	monthsAfter(d: string, locale?: string): Date;
+	yearAfter(d: string, locale?: string): Date;
+	yearAfter(d: Date, locale?: string): Date;
+	yearsAfter(d: string, locale?: string): Date;
+	yearsAfter(d: Date, locale?: string): Date;
+
+	/***
+	* @short Returns a date that is <n> units ago.
+	* @method [unit]Ago()
+	* @returns Date
+	* @extra Note that "months" is ambiguous as a unit of time.
+	*        If the target date falls on a day that does not exist
+	*        (ie. August 31 -> February 31), the date will be shifted
+	*        to the last day of the month. Be careful using %monthsAgo% 
+	*        if you need exact precision.
+	*
+	* @set
+	*   millisecondAgo
+	*   millisecondsAgo
+	*   secondAgo
+	*   secondsAgo
+	*   minuteAgo
+	*   minutesAgo
+	*   hourAgo
+	*   hoursAgo
+	*   dayAgo
+	*   daysAgo
+	*   weekAgo
+	*   weeksAgo
+	*   monthAgo
+	*   monthsAgo
+	*   yearAgo
+	*   yearsAgo
+	*
+	* @example
+	*
+	*   (5).weeksAgo() -> 5 weeks ago
+	*   (1).yearAgo()  -> January 23, 1996
+	*
+	***/
+	millisecondAgo(): Date;
+	millisecondsAgo(): Date;
+	secondAgo(): Date;
+	secondsAgo(): Date;
+	minuteAgo(): Date;
+	minutesAgo(): Date;
+	hourAgo(): Date;
+	hoursAgo(): Date;
+	dayAgo(): Date;
+	daysAgo(): Date;
+	weekAgo(): Date;
+	weeksAgo(): Date;
+	monthAgo(): Date;
+	monthsAgo(): Date;
+	yearAgo(): Date;
+	yearsAgo(): Date;
+
+	/***
+	* @short Returns a date that is <n> units before [d], where <n> is the number.
+	* @method [unit]Before([d], [locale] = currentLocale)
+	* @returns Date
+	* @extra [d] will accept a date object, timestamp, or text format.
+	*        Note that "months" is ambiguous as a unit of time. If the
+	*        target date falls on a day that does not exist
+	*        (ie. August 31 -> February 31), the date will be shifted to
+	*        the last day of the month. Be careful using %monthsBefore%
+	*        if you need exact precision. See @date_format for more.
+	*
+	* @set
+	*   millisecondBefore
+	*   millisecondsBefore
+	*   secondBefore
+	*   secondsBefore
+	*   minuteBefore
+	*   minutesBefore
+	*   hourBefore
+	*   hoursBefore
+	*   dayBefore
+	*   daysBefore
+	*   weekBefore
+	*   weeksBefore
+	*   monthBefore
+	*   monthsBefore
+	*   yearBefore
+	*   yearsBefore
+	*
+	* @example
+	*
+	*   (5).daysBefore('tuesday')          -> 5 days before tuesday of this week
+	*   (1).yearBefore('January 23, 1997') -> January 23, 1996
+	*
+	***/
+	millisecondBefore(d: string, locale?: string): Date;
+	millisecondBefore(d: Date, locale?: string): Date;
+	millisecondsBefore(d: string, locale?: string): Date;
+	millisecondsBefore(d: Date, locale?: string): Date;
+	secondBefore(d: string, locale?: string): Date;
+	secondBefore(d: Date, locale?: string): Date;
+	secondsBefore(d: string, locale?: string): Date;
+	secondsBefore(d: Date, locale?: string): Date;
+	minuteBefore(d: string, locale?: string): Date;
+	minuteBefore(d: Date, locale?: string): Date;
+	minutesBefore(d: string, locale?: string): Date;
+	minutesBefore(d: Date, locale?: string): Date;
+	hourBefore(d: string, locale?: string): Date;
+	hourBefore(d: Date, locale?: string): Date;
+	hoursBefore(d: string, locale?: string): Date;
+	hoursBefore(d: Date, locale?: string): Date;
+	dayBefore(d: string, locale?: string): Date;
+	dayBefore(d: Date, locale?: string): Date;
+	daysBefore(d: string, locale?: string): Date;
+	daysBefore(d: Date, locale?: string): Date;
+	weekBefore(d: string, locale?: string): Date;
+	weekBefore(d: Date, locale?: string): Date;
+	weeksBefore(d: string, locale?: string): Date;
+	weeksBefore(d: Date, locale?: string): Date;
+	monthBefore(d: string, locale?: string): Date;
+	monthBefore(d: Date, locale?: string): Date;
+	monthsBefore(d: string, locale?: string): Date;
+	monthsBefore(d: Date, locale?: string): Date;
+	yearBefore(d: string, locale?: string): Date;
+	yearBefore(d: Date, locale?: string): Date;
+	yearsBefore(d: string, locale?: string): Date;
+	yearsBefore(d: Date, locale?: string): Date;
+
+	/***
+	* @short Returns a date <n> units from now.
+	* @method [unit]FromNow()
+	* @returns Date
+	* @extra Note that "months" is ambiguous as a unit of time.
+	*        If the target date falls on a day that does not exist
+	*        (ie. August 31 -> February 31), the date will be shifted
+	*        to the last day of the month. Be careful using %monthsFromNow%
+	*        if you need exact precision.
+	*
+	* @set
+	*   millisecondFromNow
+	*   millisecondsFromNow
+	*   secondFromNow
+	*   secondsFromNow
+	*   minuteFromNow
+	*   minutesFromNow
+	*   hourFromNow
+	*   hoursFromNow
+	*   dayFromNow
+	*   daysFromNow
+	*   weekFromNow
+	*   weeksFromNow
+	*   monthFromNow
+	*   monthsFromNow
+	*   yearFromNow
+	*   yearsFromNow
+	*
+	* @example
+	*
+	*   (5).weeksFromNow() -> 5 weeks ago
+	*   (1).yearFromNow()  -> January 23, 1998
+	*
+	***/
+	millisecondFromNow(): Date;
+	millisecondsFromNow(): Date;
+	secondFromNow(): Date;
+	secondsFromNow(): Date;
+	minuteFromNow(): Date;
+	minutesFromNow(): Date;
+	hourFromNow(): Date;
+	hoursFromNow(): Date;
+	dayFromNow(): Date;
+	daysFromNow(): Date;
+	weekFromNow(): Date;
+	weeksFromNow(): Date;
+	monthFromNow(): Date;
+	monthsFromNow(): Date;
+	yearFromNow(): Date;
+	yearsFromNow(): Date;
+
+	/***
+	* @short Returns an array containing numbers from the number up to <num>.
+	* @method upto(<num>, [fn], [step] = 1)
+	* @returns Array
+	* @extra Optionally calls [fn] callback for each number in that array.
+	*       [step] allows multiples greater than 1.
+	* @example
+	*
+	*   (2).upto(6) -> [2, 3, 4, 5, 6]
+	*   (2).upto(6, function(n) {
+	*     // This function is called 5 times receiving n as the value.
+	*   });
+	*   (2).upto(8, null, 2) -> [2, 4, 6, 8]
+	*
+	***/
+	upto(num: number, fn?: Function, step?: number): number[];
+}
+
+
